fe0d79c249d207f253da71cba98aa64c2fa7f394
osmed00 <omar_sayed00@hotmail.com>
2020-02-01 12:02:04 +0200

    message

---
+++ SRC/MCAL/DIO/Dio.h
@@ -0,0 +1,14 @@
+
+#ifndef DIO_H_
+#define DIO_H_
+
+#include "Types.h"
+#include "Dio_cfg.h"
+
+
+extern void  Dio_WritePin (Dio_port_t port_num, uint8 pin_num, uint8 Level);
+extern uint8 Dio_ReadPin  (Dio_port_t port_num, uint8 pin_num);
+extern void  Dio_Init     (void);
+
+
+#endif /* DIO_H_ */
